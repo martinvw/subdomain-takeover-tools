e6e4006c2b27e49a315ad40a6de3596e4fdc0710
Martin van Wingerden <martin@martinvw.nl>
2022-01-11 10:41:57 +0100

    Add command line calls for Azure and release pipeline

--- setup.py
+++ setup.py
@@ -10,18 +10,43 @@ def get_metadata():
         return dict(re.findall("__([a-z]+)__ = ['\"]([^'\"]+)['\"]", f.read()))
 
 
+def update_version(target_version):
+    with open(os.path.join("subdomain_takeover_tools", "__init__.py"), 'w') as f:
+        for key, value in metadata.items():
+            if key == 'version':
+                value = target_version
+
+            f.write("__%s__ = '%s'\n" % (key, value))
+
+
 metadata = get_metadata()
 
 if sys.argv[-1] == 'publish':
     # os.system('cd docs && make html')
-    os.system('python setup.py sdist')
+    os.system('rm -rf dist')
+    os.system('rm -rf build')
+    os.system('python3 -m build')
 
-    print("You probably want to upload it to pypi:")
-    print("  twine upload dist/* -p")
+    sys.stdout.write('Continue with publishing and committing? ')
+    input()
 
-    print("You probably want to also tag the version now:")
-    print("  git tag -a %s -m 'version %s'" % (metadata['version'], metadata['version']))
-    print("  git push --tags")
+    print("Uploading release to pypi:")
+    os.system("python3 -m twine upload --repository pypi dist/subdomain_takeover_tools-%s* " % metadata['version'])
+
+    print("Tagging:")
+    os.system("git tag -a %s -m 'version %s'" % (metadata['version'], metadata['version']))
+    os.system("git push --tags")
+
+    print("Upgrading dev version:")
+    from bump import SemVer
+
+    version = SemVer.parse(metadata['version'])
+    version.bump(minor=True, reset=True)
+    update_version(str(version))
+    os.system("git add subdomain_takeover_tools/__init__.py")
+    os.system("git commit -m \"New development version\"")
+    os.system("git push")
+
     sys.exit()
 
 try:
@@ -43,14 +68,25 @@ setup(
     entry_points={'console_scripts': ['confirm_elb=subdomain_takeover_tools.confirm_elb:main',
                                       'confirm_s3=subdomain_takeover_tools.confirm_s3:main',
                                       'confirm_shopify=subdomain_takeover_tools.confirm_shopify:main',
+                                      'confirm_azure_app_service=subdomain_takeover_tools.confirm_azure_app_service'
+                                      ':main',
+                                      'confirm_azure_traffic_manager=subdomain_takeover_tools'
+                                      '.confirm_azure_traffic_manager:main',
                                       'extract_domain_names=subdomain_takeover_tools.extract_domain_names:main',
                                       ]},
     include_package_data=True,
     install_requires=[
         'tldextract',
         'dnspython',
-        'boto3'
+        'boto3',
+        'azure-identity',
+        'azure-mgmt-trafficmanager'
     ],
+    extras_require={
+        'dev': [
+            'bump'
+        ]
+    },
     keywords=['subdomain-takeover', 'subtake', 'elb', 's3', 'shopify'],
     classifiers=[
         'Environment :: Console',
