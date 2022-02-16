cc96eea2495b282d875395de5114f9fb8e36e4f4
Martin van Wingerden <martin@martinvw.nl>
2022-02-16 19:55:02 +0100

    Added confirmation for pantheon

---
+++ subdomain_takeover_tools/confirm_pantheon.py
@@ -0,0 +1,32 @@
+from subdomain_takeover_tools.helper.main import bootstrap
+from subdomain_takeover_tools.helper.prepare import prepare_domain_name
+
+
+def is_valid(_, cname):
+    if cname is None:
+        return False
+
+    return confirm_pantheon(cname)
+
+
+def confirm_pantheon(cname):
+    cname = prepare_domain_name(cname)
+
+    if check_postfix(cname) and cname.count('.') == 2:
+        (prefix, _, _) = cname.split('.')
+
+        return prefix.startswith('dev-') or prefix.startswith('test-') or prefix.startswith(
+            'test2-') or prefix.startswith('live-') or prefix.startswith('stage-') or prefix.startswith('pre-live-')
+    return False
+
+
+def check_postfix(cname):
+    return cname.endswith('getpantheon.com') or cname.endswith('gotpantheon.com') or cname.endswith('pantheonsite.io')
+
+
+def main():
+    bootstrap(is_valid)
+
+
+if __name__ == "__main__":
+    main()
