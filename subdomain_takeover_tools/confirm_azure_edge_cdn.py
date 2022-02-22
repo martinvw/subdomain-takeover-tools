bcf9bd13206497572b9b4459745a2597aa5f00b3
Martin van Wingerden <martin@martinvw.nl>
2022-02-22 22:51:37 +0100

    Add additional checks:
    - azure edge/cdn

---
+++ subdomain_takeover_tools/confirm_azure_edge_cdn.py
@@ -0,0 +1,40 @@
+import requests
+from azure.identity import DefaultAzureCredential
+
+from subdomain_takeover_tools.helper.main import bootstrap
+from subdomain_takeover_tools.helper.prepare import resolve_cname
+
+EDGE_CDN = '.azureedge.net'
+
+credential = DefaultAzureCredential()
+
+session = requests.Session()
+(token, _) = credential.get_token('https://management.azure.com/.default')
+session.headers['Authorization'] = "Bearer " + token
+url = "https://management.azure.com/providers/Microsoft.Cdn/checkNameAvailability?api-version=2019-12-31"
+
+
+def is_valid(hostname, cname):
+    if hostname == cname:
+        cname = resolve_cname(hostname)
+
+    if cname is None:
+        return False
+
+    return confirm_azure_edge_cdn(cname)
+
+
+def confirm_azure_edge_cdn(cname):
+    if cname.count('.') == 2 and EDGE_CDN in cname:
+        dns_prefix = cname.replace(EDGE_CDN, '')
+        result = session.post(url, json={'name': dns_prefix, 'type': 'Microsoft.Cdn/Profiles/Endpoints'})
+        return result.json()['nameAvailable']
+    return False
+
+
+def main():
+    bootstrap(is_valid)
+
+
+if __name__ == "__main__":
+    main()
